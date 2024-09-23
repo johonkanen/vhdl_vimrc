cd4f48186a6e717c0ba73e5183fbf27cc3a71867
jari <jari.honkanen@danfoss.com>
2024-09-23 14:32:15 +0300

    add harpoon and keymaps for horz and vert splits

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,11 +1,16 @@
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>vs", vim.cmd.vs)
+vim.keymap.set("n", "<leader>vp", vim.cmd.sp)
 
 
 -- map alt+hjkl to move between splits
--- local opts = { noremap = true, silent = true }
--- vim.api.nvim_set_keymap('n', '<A-h>', '<C-w>h', opts)
--- vim.api.nvim_set_keymap('n', '<A-j>', '<C-w>j', opts)
--- vim.api.nvim_set_keymap('n', '<A-k>', '<C-w>k', opts)
--- vim.api.nvim_set_keymap('n', '<A-l>', '<C-w>l', opts)
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap('n', '<A-h>', '<C-w>h', opts)
+vim.api.nvim_set_keymap('n', '<A-j>', '<C-w>j', opts)
+vim.api.nvim_set_keymap('n', '<A-k>', '<C-w>k', opts)
+vim.api.nvim_set_keymap('n', '<A-l>', '<C-w>l', opts)
+
+
+

c06f0ac2fce4869c0e3e5610c27f6612b00a1994
jari <jari.honkanen@danfoss.com>
2024-09-23 11:40:59 +0300

    add keymaps

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 require("set")
+require("keymaps")
 require("config.lazy")
---
+++ lua/keymaps.lua
@@ -0,0 +1,11 @@
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+-- map alt+hjkl to move between splits
+-- local opts = { noremap = true, silent = true }
+-- vim.api.nvim_set_keymap('n', '<A-h>', '<C-w>h', opts)
+-- vim.api.nvim_set_keymap('n', '<A-j>', '<C-w>j', opts)
+-- vim.api.nvim_set_keymap('n', '<A-k>', '<C-w>k', opts)
+-- vim.api.nvim_set_keymap('n', '<A-l>', '<C-w>l', opts)

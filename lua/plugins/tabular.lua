dc32bc1525208d6d6b22994eaadb30dc77b8984a
Jari Honkanen <jaarhonka@yahoo.com>
2024-10-08 07:43:29 +0300

    add tabular

---
+++ lua/plugins/tabular.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'godlygeek/tabular',
+    config = function()
+      -- Map <S-Tab> to trigger the Tabular command
+      vim.keymap.set('n', '<S-Tab>', function()
+        -- This simulates entering command mode and typing :Tabularize/Tab
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(":Tab ", true, false, true), 'n', false)
+      end, { noremap = true, silent = true })
+    end,
+  },
+}
